3c742252263d055d89738c4dd887a306a26f036b
Hugh Bagan <hbagan@ualberta.ca>
2018-02-10 19:37:01 -0700

    Added comments to ModBattleScene.js

--- js/plugins/ModBattleScene.js
+++ js/plugins/ModBattleScene.js
@@ -3,23 +3,32 @@
 //=============================================================================
  
 /*:
- * @plugindesc Modifies the battle window.
+ * @plugindesc Modifies the battle scene for our conversation battles.
  * @author Hugh Bagan
  *
- * @help
+ * @help There are no plugin commands.
  * 
  */
- 
-BattleManager.displayStartMessages = function() { 
+
+/*  Taken straight from the source and redefined here.
+ *
+ */
+BattleManager.displayStartMessages = function() {
+
     $gameTroop.enemyNames().forEach(function(name) {
-        // Execute this function on each enemy name in the troop
-        if (name == "Richard") {
+        // Execute this function on each enemy name in the troop.
+        // Put your chosen emerge text for an enemy here.
+        if (name == "Name_of_Enemy") {                          // Example
+            $gameMessage.add("It's coming straight for us!!");
+        }
+        else if (name == "Richard") {
             $gameMessage.add("TA Richard is haranguing you for homework!");
         }
         else {
+            // Failsafe
             $gameMessage.add(TextManager.emerge.format(name));
         }
-    }); 
+    });
     if (this._preemptive) { 
         $gameMessage.add(TextManager.preemptive.format($gameParty.name())); 
     } else if (this._surprise) { 
